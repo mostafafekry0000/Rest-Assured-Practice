5bd8be3d5841b1d9d8b34b8efdae74c7511e0e83
Mostafa Fekry <166963552+mostafafekry0000@users.noreply.github.com>
2024-06-29 20:12:34 +0300

    Add files via upload

---
+++ getRequest.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+
+public class getRequest {
+
+    public static void main(String[] args) {
+
+        RestAssured
+                .given()
+                .baseUri("https://reqres.in/api/users")
+                .queryParam("page","2")
+                .when()
+                .get()
+                .prettyPrint();
+
+    }
+}
